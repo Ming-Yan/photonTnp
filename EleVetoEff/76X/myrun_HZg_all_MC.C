62307e4b69a5e3df212dca4b803127abe9917977
Victoria <victoria.louise.quilatan@cern.ch>
2016-07-13 10:50:35 +0200

    run xAna.cc

---
+++ EleVetoEff/76X/myrun_HZg_all_MC.C
@@ -0,0 +1,37 @@
+{
+
+	gSystem->AddIncludePath("-Iexternal");
+	gSystem->SetBuildDir("tmpdir", kTRUE);
+	//gROOT->LoadMacro("xAna.cc+");
+	gROOT->ProcessLine(".L xAna.cc+");
+
+	TString outpath = "mytry.root";
+
+
+
+	const int nfiles = 5; //number of files
+
+	// TString inpath[nfiles] = {"/data6/ggNtuples/V07_04_14_01/job_spring15_DYJetsToLL_m50_miniAOD.root","/data2/shilpi/CD_25ns/job_DoubleMu_Run2015CD_miniAOD.root", "/data2/shilpi/DY_TnP/ggtree_DYJets_madgraph.root"}; 
+	TString inpath[nfiles] = { "/data3/ggNtuples/V07_06_03_00/job_DoubleMu_Run2015C_Dec16_miniAOD.root", "/data3/ggNtuples/V07_06_03_00/job_DoubleMu_Run2015D_Dec16_miniAOD.root", "/data6/ggNtuples/V07_06_03_00/job_fall15_DYJetsToLL_m50_miniAOD.root", "/data6/ggNtuples/V07_06_03_00/job_fall15_WJetsToLNu_aMCatNLO_miniAOD.root", "/data6/ggNtuples/V07_06_03_00/job_fall15_TT_powheg_miniAOD.root" };
+
+	// char* outfile[nfiles] = {"DYJets.root","data.root", "DYJets_mad.root"};
+	char* outfile[nfiles] = { "dataC.root", "dataD.root", "signal.root", "WgBkg.root", "TTBkg.root"};
+	// char* type[nfiles] = {"signal","data", "signal"};
+	char* type[nfiles] = { "data", "data", "signal", "WgBkg", "TTBkg" };
+
+	for (int i = 0; i < nfiles; i++){
+
+		const char* inpaths[] = { inpath[i].Data() };
+
+		cout << "RUNNIGN FILE " << inpath[i].Data() << " AND OUTPUT FILE IS " << outfile[i] << endl;
+
+		int qcdchannel = 0; //for photons + jets
+		const char* out = outfile[i];
+
+		const char* ftype = type[i];
+		xAna(inpaths, 1, out, ftype, 0, -1/*, 1, qcdchannel*/);
+	}//for(int i=0; i<nfiles; i++)
+
+
+
+}
